8d87134dd473755ffc7e0a21ccd215fd9eb4e361
charity <charity23000@student.hamk.fi>
2024-01-29 13:02:18 +0200

    exer2 completed

---
+++ cjk-tasks/cjk-task2/footer.php
@@ -0,0 +1,9 @@
+<footer>
+
+   
+    <p>
+        company ltd<br>
+        business street 33 <br>
+        00100 helsinki
+    </p>
+</footer>
---
+++ cjk-tasks/cjk-task2/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="vie wport" content="width=device-width, initial-scale=1.0">
+    <title>Home - news</title>
+    <link rel="stylesheet"href="styles.css">
+</head>
+<body>
+    <div class="container">
+    <header>
+
+        <h1 class="logo">News</h1>
+<nav>
+    <ul>
+        <li><a href="ex1.php">exercise 1</a></li>
+        <li><a href="ex2.php">exercise 2</a></li>
+        
+</nav>
+    </header>
+    <article>
